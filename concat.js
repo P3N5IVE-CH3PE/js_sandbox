8819413651705adf77958d89290d486f5f4102b6
P3N5IVE_CH3PE <P3N5IVE_CH3PE@protonmail.com>
2023-02-09 15:35:25 -0800

    update code

--- concat.js
+++ concat.js
@@ -1,6 +1,7 @@
 const primerNombre = "John";
 const lastName = "Doe";
 const age = 32;
+const str = 'Hello my name is bob';
 
 let val;
 
@@ -18,6 +19,33 @@ val = "Hello my names is " + primerNombre + " and my age is " + age;
 //Escaping
 val = 'That\'s awesome I can\'t wait!';
 
+//length
+val = primerNombre.length;
+
+//concat()
+val = primerNombre.concat(' ', lastName);
+
+//change case
+val = primerNombre.toUpperCase();
+val = primerNombre.toLowerCase();
+
+//Array
+val = primerNombre[0];
+
+//indef of
+val = primerNombre.indexOf('J');
+
+//Get last char()
+val = primerNombre.charAt(primerNombre.length - 1);
+
+//substring()
+val = primerNombre.substring(0, 2);
+
+//slice()
+val = primerNombre.slice(0,2);
+
+//split()
+val = str.split(' ');
 
 console.log(val);
 
